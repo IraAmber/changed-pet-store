29c6941a33da9ecde23065c02fddfd8623d921fc
Iryna Kuziv <nute7021@gmail.com>
2023-09-10 20:45:53 +0300

    fixed bugs with api

--- src/api/getCardsList.js
+++ src/api/getCardsList.js
@@ -1,13 +1,30 @@
-// Ця функція отримує список карток з сервера
-export const getCardsList = async () => {
-  // Відправляємо запит на сервер для отримання списку карток
-  const response = await fetch('../../public/cardList.json')
+// // Ця функція отримує список карток з сервера
+// export const getCardsList = async () => {
+//   // Відправляємо запит на сервер для отримання списку карток
+//   const response = await fetch('../../public/cardList.json')
 
-  // Перевіряємо, чи запит був успішним (статус 200 OK)
-  if (!response.ok) {
-    throw new Error('Помилка - ' + response.status) // Якщо статус не 200 OK, викидаємо помилку з відповідним повідомленням
-  }
+//   // Перевіряємо, чи запит був успішним (статус 200 OK)
+//   if (!response.ok) {
+//     throw new Error('Помилка - ' + response.status) // Якщо статус не 200 OK, викидаємо помилку з відповідним повідомленням
+//   }
 
-  // Повертаємо дані у форматі JSON
-  return response.json()
+//   // Повертаємо дані у форматі JSON
+//   return response.json()
+// }
+
+import axios from 'axios'
+
+export const getCardsList = () => {
+  return new Promise((resolve, reject) => {
+    axios
+      .get('./cardList.json')
+      .then((response) => {
+        const cards = response.data
+        resolve(cards) // Викликаємо resolve() з отриманими даними
+      })
+      .catch((error) => {
+        console.error('Помилка отримання списку карток', error)
+        reject(error) // Викликаємо reject() у разі помилки
+      })
+  })
 }
